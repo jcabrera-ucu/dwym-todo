d31db919d5e3f641457e1ea922bf8e70bcffeeb6
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2023-06-05 20:45:52 -0300

    Add basic data types and get endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,45 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+interface User {
+  id: number
+  name: string
+}
+
+interface Task {
+  id: number
+  description: string
+  done: boolean
+}
+
+interface UsersDb {
+  [userId: number]: User
+}
+
+interface TasksDb {
+  [userId: number]: Task[]
+}
+
+const users: UsersDb = {
+  1: {
+    id: 1,
+    name: 'Pepe',
+  },
+  2: {
+    id: 2,
+    name: 'Paco',
+  },
+}
+
+const tasks: TasksDb = {
+  1: [ ],
+  2: [ ],
+}
+
+app.get('/users/:userId/tasks', (req: Request, res: Response) => {
+  const userId = Number(req.params.userId);
+  // FIXME: Handle errors!!
+  res.send(JSON.stringify(tasks[userId]));
 });
 
 app.listen(port, () => {
