6af52ebaf781d99c949a44edfca782c37188fcc0
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2023-06-07 17:58:42 -0300

    Reformat index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,45 +7,45 @@ const app: Express = express();
 const port = 3000;
 
 const users: User[] = [
-  { id: 1, name: 'Pepe' },
-  { id: 2, name: 'Paco' },
+    {id: 1, name: 'Pepe'},
+    {id: 2, name: 'Paco'},
 ];
 
 function existsUserWithId(userId: number): boolean {
-  for (const user of users) {
-    if (user.id == userId) {
-      return true;
+    for (const user of users) {
+        if (user.id === userId) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 const tasks: Task[] = [
-  {
-    id: 1,
-    userId: 1,
-    description: 'Buy milk',
-    done: false,
-  },
+    {
+        id: 1,
+        userId: 1,
+        description: 'Buy milk',
+        done: false,
+    },
 ];
 
 function filterTasksByUserId(userId: number): Task[] {
-  return tasks.filter(x => x.userId === userId);
+    return tasks.filter(x => x.userId === userId);
 }
 
 app.get('/users/:userId/tasks', (req: Request, res: Response) => {
-  const userId = Number(req.params.userId);
+    const userId = Number(req.params.userId);
 
-  if (existsUserWithId(userId)) {
-    res.send(JSON.stringify(filterTasksByUserId(userId)));
-  } else {
-    res.status(404).send(JSON.stringify({
-      'code': ErrorCodes.UNKNOWN_USER,
-    }));
-  }
+    if (existsUserWithId(userId)) {
+        res.send(JSON.stringify(filterTasksByUserId(userId)));
+    } else {
+        res.status(404).send(JSON.stringify({
+            'code': ErrorCodes.UNKNOWN_USER,
+        }));
+    }
 });
 
 app.listen(port, () => {
-  // tslint:disable-next-line
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    // tslint:disable-next-line
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
